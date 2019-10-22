f9a52873d20299b175b166c8db2dc41aa8d33a25
ellenchuaye <chuayunen@gmail.com>
2019-10-23 01:18:51 +0800

    SearchMgr.java

---
+++ app/src/main/java/com/example/the_hawks/SearchMgr.java
@@ -0,0 +1,31 @@
+package com.example.the_hawks;
+
+public class SearchMgr {
+    private HCList hcList;
+    //  private HawkerCentre hawkerCentre;
+    public SearchManager(){
+        HCList temp = new HCList();
+        this.hcList = temp.getHCList();
+    }
+
+
+    public HCList getLocation(float xcoord, float ycoord){
+
+    }
+
+    public HCList getName(String name){
+
+    }
+
+    public HCList filterList(float rating, float xcoord, float ycoord){
+
+    }
+
+    public HawkerCentre selectCentre(String name){
+
+    }
+
+    public void selectNavigation(boolean walk, boolean transit, boolean drive){
+
+    }
+}
