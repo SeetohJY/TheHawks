0d50b6f4b9f9f8edec46dae85ba3e1e4eed0703b
Ash <ashl0005@e.ntu.edu.sg>
2019-10-23 00:51:50 +0800

    HawkerMgr skeleton

---
+++ app/src/main/java/com/example/the_hawks/HawkerMgr.java
@@ -0,0 +1,16 @@
+package com.example.the_hawks;
+
+public class HawkerMgr {
+    public HawkerCentre searchHawkerCentre() {
+        return HawkerCentre;
+    }
+    public void filterByType(Enum type) {
+
+    }
+    public void filterByHygieneAggregate(float aggregate) {
+
+    }
+    public void filterByName(String name) {
+
+    }
+}
