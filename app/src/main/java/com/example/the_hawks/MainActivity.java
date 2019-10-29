d2c45c979634251fd154dafe1b774af0b75d86bb
Faith <angxinjiefaith@gmail.com>
2019-10-30 02:47:13 +0800

    added button to mainactivity, edited mapsactivity

--- app/src/main/java/com/example/the_hawks/MainActivity.java
+++ app/src/main/java/com/example/the_hawks/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.the_hawks;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // wire buttonToMap
+        Button btn = (Button) findViewById(R.id.buttonToMap);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMapsActivity();
+            }
+        });
+    }
+
+    public void openMapsActivity() {
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
     }
 }
