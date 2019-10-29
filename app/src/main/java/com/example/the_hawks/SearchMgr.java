61afe23a1f5b6b3f53a6cc4e486b2bf010077eb5
felyssechua <fely0001@e.ntu.edu.sg>
2019-10-30 03:15:18 +0800

    search manager

--- app/src/main/java/com/example/the_hawks/SearchMgr.java
+++ app/src/main/java/com/example/the_hawks/SearchMgr.java
@@ -1,30 +1,47 @@
-//package com.example.the_hawks;
-//public class SearchMgr {
-//    private HCList hcList;
-//    //  private HawkerCentre hawkerCentre;
-//    public void SearchManager(){
-//        HCList temp = new HCList();
-//        this.hcList = temp.getHCList();
-//    }
-//
-//
-//    public HCList getLocation(float xcoord, float ycoord){
-//        return hcList;
-//    }
-//
-//    public HCList getName(String name){
-//        return hcList;
-//    }
-//
-//    public HCList filterList(float rating, float xcoord, float ycoord){
-//        return hcList;
-//    }
-//
-//    public HawkerCentre selectCentre(String name){
-//        return hcList;
-//    }
-//
-//    public void selectNavigation(boolean walk, boolean transit, boolean drive){
-//
-//    }
-//}
+
+package com.example.the_hawks;
+
+public class SearchMgr {
+    private HCList hcList;
+
+    //  private HawkerCentre hawkerCentre;
+
+    //constructor
+    public void SearchManager() {
+        HCList temp = new HCList();
+        this.hcList = temp.getHCList();
+    }
+
+
+    public HCList getLocation(float xcoord, float ycoord) {
+        return hcList;
+    }
+
+    public HCList getName(String name) {
+        new HCList();
+        HCList byName;
+        for (String HCname : hcList) {
+            if (HCname.contains(name)) {
+                byName = (HCList) ((HCList) hcList).clone();
+                return byName;
+            }
+        }
+    }
+
+    public HCList filterList(float rating, float xcoord, float ycoord) {
+        return hcList;
+    }
+
+    public HawkerCentre selectCentre(String name) {
+        for (String HCname : hcList) {
+            if (HCname.contains(name)) {
+                return hcList.HawkerCentre;
+            }
+        }
+
+        public void selectNavigation(boolean walk, boolean transit, boolean drive){
+            
+        }
+    }
+}
+
