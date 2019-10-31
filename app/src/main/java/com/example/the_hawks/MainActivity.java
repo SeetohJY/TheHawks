8292e37512d0f6f2f7d155161f9bb0fd1b9dec2a
Ash <ashl0005@e.ntu.edu.sg>
2019-11-01 00:32:09 +0800

    MapsActivity

--- app/src/main/java/com/example/the_hawks/MainActivity.java
+++ app/src/main/java/com/example/the_hawks/MainActivity.java
@@ -1,17 +1,11 @@
 package com.example.the_hawks;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 
-
-import android.view.View;
-import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private Button rollButton;
@@ -21,22 +15,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        final Button stalls = findViewById(R.id.startStalls);
-        stalls.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                startStalls();
-            }
-        });
+// figure out whats wrong with the buttons
+//        final Button stalls = findViewById(R.id.startStalls);
+//        stalls.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                startStalls();
+//            }
+//        });
 
         // wire buttonToMap
-        Button btn = (Button) findViewById(R.id.buttonToMap);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openMapsActivity();
-            }
-        });
+//        Button btn = (Button) findViewById(R.id.buttonToMap);
+//        btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                openMapsActivity();
+//            }
+//        });
 
 
         rollButton = (Button) findViewById(R.id.rollButton);
--- app/src/main/java/com/example/the_hawks/SearchMgr.java
+++ app/src/main/java/com/example/the_hawks/SearchMgr.java
@@ -1,47 +1,48 @@
-import com.example.the_hawks.HCList;
-import com.example.the_hawks.HawkerCenter;
-package com.example.the_hawks;
-
-public class SearchMgr {
-    private HCList hcList;
-
-    //  private HawkerCentre hawkerCentre;
-
-    //constructor
-    public void SearchManager() {
-        HCList temp = new HCList();
-        this.hcList = temp.getHCList();
-    }
-
-
-    public HCList getLocation(float xcoord, float ycoord) {
-        return hcList;
-    }
-
-    public HCList getName(String name) {
-        new HCList();
-        HCList byName;
-        for (String HCname : hcList) {
-            if (HCname.contains(name)) {
-                byName = (HCList) ((HCList) hcList).clone();
-                return byName;
-            }
-        }
-    }
-
-    public HCList filterList(float rating, float xcoord, float ycoord) {
-        return hcList;
-    }
-
-    public HawkerCentre selectCentre(String name) {
-        for (String HCname : hcList) {
-            if (HCname.contains(name)) {
-                return hcList.HawkerCentre;
-            }
-        }
-
-        public void selectNavigation(boolean walk, boolean transit, boolean drive){
-            
-        }
-    }
-}
+//package com.example.the_hawks;
+//import com.example.the_hawks.HCList;
+//import com.example.the_hawks.HawkerCenter;
+//
+//
+//public class SearchMgr {
+//    private HCList hcList;
+//
+//    //  private HawkerCentre hawkerCentre;
+//
+//    //constructor
+//    public void SearchManager() {
+//        HCList temp = new HCList();
+//        this.hcList = temp.getHCList();
+//    }
+//
+//
+//    public HCList getLocation(float xcoord, float ycoord) {
+//        return hcList;
+//    }
+//
+//    public HCList getName(String name) {
+//        new HCList();
+//        HCList byName;
+//        for (String HCname : hcList) {
+//            if (HCname.contains(name)) {
+//                byName = (HCList) ((HCList) hcList).clone();
+//                return byName;
+//            }
+//        }
+//    }
+//
+//    public HCList filterList(float rating, float xcoord, float ycoord) {
+//        return hcList;
+//    }
+//
+//    public HawkerCentre selectCentre(String name) {
+//        for (String HCname : hcList) {
+//            if (HCname.contains(name)) {
+//                return hcList.HawkerCentre;
+//            }
+//        }
+//
+//        public void selectNavigation(boolean walk, boolean transit, boolean drive){
+//
+//        }
+//    }
+//}
